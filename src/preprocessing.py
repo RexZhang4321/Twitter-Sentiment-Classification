15f4512fa2f061a9f4fae8c5415d8ec811c9cf8f
rexz <rexzhang4321@gmail.com>
2016-11-26 14:32:24 -0500

    fine tuned preprocessing

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -8,7 +8,6 @@ from nltk.corpus import stopwords
 
 stops = set(stopwords.words("english"))
 punctuation = string.punctuation
-ascii = string.ascii_lowercase
 stemmer = nltk.stem.porter.PorterStemmer()
 
 
@@ -20,17 +19,103 @@ def load_data(path, names=None, usecols=None):
 
 def parse_row(row):
     _row = []
+    row = re.sub(hash_regex, hash_repl, row)
+    row = re.sub(hndl_regex, hndl_repl, row)
+    row = re.sub(url_regex, "__URL", row)
+    for (repl, regx) in emoticons_regex:
+        row = re.sub(regx, ' ' + repl + ' ', row)
+    row = row.replace('\'', '')
+    row = re.sub(word_bound_regex, punctuations_repl, row)
+    row = re.sub(rpt_regex, rpt_repl, row)
     try:
         for word in word_tokenize(row):
-            if word in stops or len(word) == 1:
-                    continue
-            else:
-                word = word.lower()
-                word = stemmer.stem(word)
-                _row.append(word)
+            # not pretty sure whether we need stopwords
+            word = word.lower()
+            word = stemmer.stem(word)
+            _row.append(word)
+            # if word in stops or len(word) == 1:
+            #         continue
+            # else:
+            #     word = word.lower()
+            #     word = stemmer.stem(word)
+            #     _row.append(word)
     except:
         pass
     return _row
+
+
+# Hashtags
+hash_regex = re.compile(r"#(\w+)")
+def hash_repl(match):
+    return '__HASH_'+match.group(1).upper()
+
+
+# Handels
+hndl_regex = re.compile(r"@(\w+)")
+def hndl_repl(match):
+    return '__HNDL'#_'+match.group(1).upper()
+
+
+# URLs
+url_regex = re.compile(r"(http|https|ftp)://[a-zA-Z0-9\./]+")
+
+
+# Spliting by word boundaries
+word_bound_regex = re.compile(r"\W+")
+
+
+# Repeating words like hurrrryyyyyy
+rpt_regex = re.compile(r"(.)\1{1,}", re.IGNORECASE);
+def rpt_repl(match):
+    return match.group(1)+match.group(1)
+
+
+# Emoticons
+emoticons = [
+        ('__EMOT_SMILEY',   [':-)', ':)', '(:', '(-:', ]),
+        ('__EMOT_LAUGH',    [':-D', ':D', 'X-D', 'XD', 'xD', ]),
+        ('__EMOT_LOVE',     ['<3', ':\*', ]),
+        ('__EMOT_WINK',		[';-)', ';)', ';-D', ';D', '(;', '(-;', ]),
+        ('__EMOT_FROWN',    [':-(', ':(', '(:', '(-:', ]),
+        ('__EMOT_CRY',      [':,(', ':\'(', ':"(', ':((']),
+    ]
+
+
+# Punctuations
+punctuations = [
+        #('',   ['.', ] ),\
+        #('',		[',', ] )	,\
+        #('',		['\'', '\"', ] )	,\
+        ('__PUNC_EXCL',     ['!', ]),\
+        ('__PUNC_QUES',     ['?', ]),\
+        ('__PUNC_ELLP',     ['...', ]),\
+    ]
+
+
+# For emoticon regexes
+def escape_paren(arr):
+    return [text.replace(')', '[)}\]]').replace('(', '[({\[]') for text in arr]
+
+
+def regex_union(arr):
+    return '(' + '|'.join(arr) + ')'
+
+emoticons_regex = [(repl, re.compile(regex_union(escape_paren(regx))))
+                   for (repl, regx) in emoticons]
+
+
+# For punctuation replacement
+def punctuations_repl(match):
+    text = match.group(0)
+    repl = []
+    for (key, parr) in punctuations:
+        for punc in parr:
+            if punc in text:
+                repl.append(key)
+    if (len(repl) > 0):
+        return ' ' + ' '.join(repl) + ' '
+    else:
+        return ' '
 
 
 def generate_dict_for_BOW(dt):
@@ -80,13 +165,5 @@ if __name__ == '__main__':
     usecols = [0, 5]
     dt = load_data(path, names=names, usecols=usecols)
     print dt
-    print "loading finished"
-    # get training set and test set in numpy representation
-    dic = generate_dict_for_BOW(dt[:100])
-    print "generate dic finished"
-    print dic
-    x_train, y_train, x_test, y_test = get_training_and_testing(dt[:100])
-    x_train = generate_BOW(x_train, dic)
-    x_test = generate_BOW(x_test, dic)
-    print len(x_train)
-    print len(x_test)
+    for row in dt['data']:
+        print parse_row(row)
